f6e0615e8e600073efed49e07a3190636156de4f
Quang Duy <justinquangduy@gmail.com>
2022-11-04 15:45:09 +0700

    Add Swagger UI

--- WebAPICoreDapper/Startup.cs
+++ WebAPICoreDapper/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebAPICoreDapper
 {
@@ -26,6 +27,12 @@ namespace WebAPICoreDapper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "DYU REST API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +48,15 @@ namespace WebAPICoreDapper
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DYU REST API V1");
+            });
+
             app.UseMvc();
         }
     }
